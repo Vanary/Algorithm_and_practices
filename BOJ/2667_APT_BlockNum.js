fd981e6595d7540610009e463daf1eb4ee9c472e
Boyoung Kim <boyoung@vanary.com>
2018-10-10 18:24:24 +0900

    Update completed solution

--- BOJ/2667_APT_BlockNum.js
+++ BOJ/2667_APT_BlockNum.js
@@ -1,18 +1,66 @@
 function solution(arr) { // O(N^2)
     const groundSize = arr[0]; //7
-    const ground = [...arr.splice(1)]; // [ [011..], [011..], ... ]
+    const ground = arr.splice(1); // [ [011..], [011..], ... ]
+    const hash = ground.map( row => new Array(7).fill(undefined) );
+    let numOfTowns = 0;
+    let numOfAPTs = 0;
+    let result = [];
 
-    const hash = 
+    function countAPTsInTown(idxI, idxJ) {
+        function tagBlock(idxI, idxJ) {
+            const targetBlock = ground[idxI][0][idxJ];
+            if (hash[idxI][idxJ] !== undefined) {
+                return
+            }
+            if (targetBlock === '0') {
+                hash[idxI][idxJ] = false;    
+                return
+            }
+            hash[idxI][idxJ] = `apt${numOfTowns}`;
+            numOfAPTs++;
+            
+            if (idxJ !== groundSize-1) {
+                tagBlock(idxI, idxJ+1);
+            }
+            if (idxJ !== 0) {
+                tagBlock(idxI, idxJ-1);
+            }
+            if (idxI !== groundSize-1) {
+                tagBlock(idxI+1, idxJ);
+            }
+            if (idxI !== 0) {
+                tagBlock(idxI-1, idxJ);
+            }
+            
+        }
+        if (hash[idxI][idxJ] !== undefined) {
+            return
+        }
+        if (ground[idxI][0][idxJ] === '0') {
+            hash[idxI][idxJ] = false;    
+            return
+        }
+        numOfTowns++;
+        tagBlock(idxI, idxJ);
 
-    function tagBlock() {}
+        result.push(numOfAPTs);
+
+        numOfAPTs = 0;
+        
+    }
+
     for (let i = 0; i < groundSize; i++) {
         for (let j = 0; j < groundSize; j++) {
-
+            countAPTsInTown(i,j);
         }
     }
+
+    console.log(`We have ${result.length} towns. Order of towns per APTs in them is ${result.sort((a,b) => a - b)}`);
 }
 
+solution([7, [`0110100`], [`0110101`], [`1110101`], [`0000111`], [`0100000`], [`0111110`], [`0111000`]]);
 
+// INPUT 
 // 7
 // 0110100
 // 0110101
@@ -21,3 +69,9 @@ function solution(arr) { // O(N^2)
 // 0100000
 // 0111110
 // 0111000
+
+// OUTPUT
+// 3
+// 7
+// 8
+// 9
