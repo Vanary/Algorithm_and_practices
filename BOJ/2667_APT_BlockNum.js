20b4b5b6b1335246bf71fabf9f396c60b2aca965
Boyoung Kim <boyoung@vanary.com>
2018-10-10 17:26:40 +0900

    Add folder for BOJ algorithm quizzes

---
+++ BOJ/2667_APT_BlockNum.js
@@ -0,0 +1,23 @@
+function solution(arr) { // O(N^2)
+    const groundSize = arr[0]; //7
+    const ground = [...arr.splice(1)]; // [ [011..], [011..], ... ]
+
+    const hash = 
+
+    function tagBlock() {}
+    for (let i = 0; i < groundSize; i++) {
+        for (let j = 0; j < groundSize; j++) {
+
+        }
+    }
+}
+
+
+// 7
+// 0110100
+// 0110101
+// 1110101
+// 0000111
+// 0100000
+// 0111110
+// 0111000
