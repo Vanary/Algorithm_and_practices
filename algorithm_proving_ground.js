16d18851d465008fb102edc05bec1b33b2edb79d
Boyoung KIM <boyoung@vanary.com>
2019-01-21 21:02:11 +0900

    Contents(Solution): Add new solution for [L2]_영어_끝말잇기

--- algorithm_proving_ground.js
+++ algorithm_proving_ground.js
@@ -1,39 +1,27 @@
 /*
-https://programmers.co.kr/learn/courses/30/lessons/12981?language=javascript
+https://programmers.co.kr/learn/courses/30/lessons/42839?language=javascript
 
-[L2]_영어_끝말잇기.js
+[L2]_소수_찾기.js
 
 [문제 설명]
 
-1부터 n까지 번호가 붙어있는 n명의 사람이 영어 끝말잇기를 하고 있습니다. 영어 끝말잇기는 다음과 같은 규칙으로 진행됩니다.
-1번부터 번호 순서대로 한 사람씩 차례대로 단어를 말합니다.
-마지막 사람이 단어를 말한 다음에는 다시 1번부터 시작합니다.
-앞사람이 말한 단어의 마지막 문자로 시작하는 단어를 말해야 합니다.
-이전에 등장했던 단어는 사용할 수 없습니다.
-한 글자인 단어는 인정되지 않습니다.
-다음은 3명이 끝말잇기를 하는 상황을 나타냅니다.
-tank → kick → know → wheel → land → dream → mother → robot → tank
-위 끝말잇기는 다음과 같이 진행됩니다.
-1번 사람이 자신의 첫 번째 차례에 tank를 말합니다.
-2번 사람이 자신의 첫 번째 차례에 kick을 말합니다.
-3번 사람이 자신의 첫 번째 차례에 know를 말합니다.
-1번 사람이 자신의 두 번째 차례에 wheel을 말합니다.
-(계속 진행)
-끝말잇기를 계속 진행해 나가다 보면, 3번 사람이 자신의 세 번째 차례에 말한 tank 라는 단어는 이전에 등장했던 단어이므로 탈락하게 됩니다.
-사람의 수 n과 사람들이 순서대로 말한 단어 words 가 매개변수로 주어질 때, 가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 구해서 return 하도록 solution 함수를 완성해주세요.
+한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
+각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
-[제한 사항]
+[제한사항]
 
-끝말잇기에 참여하는 사람의 수 n은 2 이상 10 이하의 자연수입니다.
-words는 끝말잇기에 사용한 단어들이 순서대로 들어있는 배열이며, 길이는 n 이상 100 이하입니다.
-단어의 길이는 2 이상 50 이하입니다.
-모든 단어는 알파벳 소문자로만 이루어져 있습니다.
-끝말잇기에 사용되는 단어의 뜻(의미)은 신경 쓰지 않으셔도 됩니다.
-정답은 [ 번호, 차례 ] 형태로 return 해주세요.
-만약 주어진 단어들로 탈락자가 생기지 않는다면, [0, 0]을 return 해주세요.
+numbers는 길이 1 이상 7 이하인 문자열입니다.
+numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+013은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+
 */
 
-function solution(n, words) {}
+function solution(numStr) {
+  const numArr = numStr.split('');
+  // numArr로 숫자 사용 경우의 수 다 뽑고
+  // 각 경우의 수에 소수찾기 알고리즘 적용 (filter)
+  // return 걸러진 경우의 수 갯수
+}
 
 // ====== 테스트 코드 ======
 const testModule = require('./Programmers_testing_template');
@@ -41,8 +29,8 @@ const testModule = require('./Programmers_testing_template');
 const tester = testModule.fn_test;
 const TestScenario = testModule.Class_TestScenario;
 
-testCase = [3, ['tank', 'kick', 'know', 'wheel', 'land', 'dream', 'mother', 'robot', 'tank']];
-expected = [3, 3];
+testCase = ['17'];
+expected = 3;
 tester(
   `테스트 - ${[...testCase]} should return ${expected}`,
   new TestScenario({
@@ -52,38 +40,8 @@ tester(
     assertionFn: (expectedResult, actualResult) => expectedResult.toString() === actualResult.toString(),
   }),
 );
-testCase = [
-  5,
-  [
-    'hello',
-    'observe',
-    'effect',
-    'take',
-    'either',
-    'recognize',
-    'encourage',
-    'ensure',
-    'establish',
-    'hang',
-    'gather',
-    'refer',
-    'reference',
-    'estimate',
-    'executive',
-  ],
-];
-expected = [0, 0];
-tester(
-  `테스트 - ${[...testCase]} should return ${expected}`,
-  new TestScenario({
-    givenArr: [...testCase],
-    whenFn: solution,
-    thenVal: expected,
-    assertionFn: (expectedResult, actualResult) => expectedResult.toString() === actualResult.toString(),
-  }),
-);
-testCase = [2, ['hello', 'one', 'even', 'never', 'now', 'world', 'draw']];
-expected = [1, 3];
+testCase = ['011'];
+expected = 2;
 tester(
   `테스트 - ${[...testCase]} should return ${expected}`,
   new TestScenario({
